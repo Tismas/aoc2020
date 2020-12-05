fad844f7e097a0371fe7d0a93dcb9960edace5d8
Adam Golan <adamski256@gmail.com>
2020-12-05 12:42:38 +0100

    run solution based on console argument

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use std::env;
+
 mod day1;
 mod day2;
 
 pub mod utils;
 
 fn main() {
-    day1::solve();
-    day2::solve();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        day1::solve();
+        day2::solve();
+        return;
+    }
+
+    match args[1].as_str() {
+        "day1" => day1::solve(),
+        "day2" => day2::solve(),
+        _ => println!("Incorrect day, example: day1")
+    }
 }
